45d544f1adb3f84cb37a512bcbc07fd3da8c48bf
anshuman7negi <anshumannegi777@gmail.com>
2023-08-07 17:38:08 +0530

    Create onClick function for button

--- src/components/button.js
+++ src/components/button.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Button = () => {
+const Button = ({ onClick }) => {
   const buttons = [
     'AC', '+/-', '%', '/',
     '7', '8', '9', 'x',
@@ -9,6 +10,10 @@ const Button = () => {
     '0', '.', '=',
   ];
 
+  const handleClick = (number) => {
+    onClick(number);
+  };
+
   return (
     <div className="buttons">
       {buttons.map((number) => (
@@ -16,12 +21,17 @@ const Button = () => {
           key={number}
           type="button"
           className={`btn ${['/', 'x', '-', '+', '='].includes(number) ? 'operations' : 'number'} ${number === '0' ? 'zero' : ''}`}
+          onClick={() => handleClick(number)}
         >
           {number}
         </button>
       ))}
     </div>
   );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,11 +1,30 @@
-import Button from './button.js';
+import React, { useState } from 'react';
+import Button from './button';
+import calculate from '../logic/calculate';
 
-export default function calculator() {
+export default function Calculator() {
+  const [data, setData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleData = (number) => {
+    const updatedData = calculate(data, number);
+    setData(updatedData);
+  };
+
   return (
     <div className="calculator-container">
       <div className="calculator">
-        <p className="result">0</p>
-        <Button />
+        <p className="result">
+          {data.total}
+          {' '}
+          {data.operation}
+          {' '}
+          {data.next}
+        </p>
+        <Button onClick={handleData} />
       </div>
     </div>
   );
