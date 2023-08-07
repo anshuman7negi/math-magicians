49af309cca968a6c194c6956ed5699d7c6b9937b
anshuman7negi <anshumannegi777@gmail.com>
2023-08-07 13:42:16 +0530

    Added child component of calculator

---
+++ src/components/button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Button = () => {
+  const buttons = [
+    'AC', '+/-', '%', '/',
+    '7', '8', '9', 'x',
+    '4', '5', '6', '-',
+    '1', '2', '3', '+',
+    '0', '.', '=',
+  ];
+
+  return (
+    <div className="buttons">
+      {buttons.map((number) => (
+        <button
+          key={number}
+          type="button"
+          className={`btn ${Number.isNaN(number) && number !== '.' ? 'operations' : 'number'} ${number === '0' ? 'zero' : ''}`}
+        >
+          {number}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Button;
